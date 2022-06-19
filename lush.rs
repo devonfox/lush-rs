86feac2302baddb264b7ef11bcf485487569ddad
devonfox <devonhouston.fox@gmail.com>
2022-06-19 11:57:46 -0700

    now accepting midi

--- lush.rs
+++ lush.rs
@@ -1,11 +1,14 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use lush_rs::Key;
+use midir::{Ignore, MidiInput, MidiInputPort};
 use rand::Rng;
-use std::io::stdin;
+use std::error::Error;
+use std::io::{stdin, stdout, Write};
 use std::sync::mpsc::*;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread::spawn;
-
-use lush_rs::Key;
+use wmidi::MidiMessage;
+use wmidi::MidiMessage::*;
 
 fn main() -> anyhow::Result<()> {
     // Find default output host
@@ -18,18 +21,63 @@ fn main() -> anyhow::Result<()> {
     println!("Output device: {:?}", device.name()?); // print result
 
     let end_chan: (Sender<()>, Receiver<()>) = channel(); // end channel
+    let note_chan: (Sender<Key>, Receiver<Key>) = channel(); // end channel
 
     let config = device.default_output_config().unwrap();
     println!("Default output config: {:?}", config);
 
-    let key: usize = rand::thread_rng().gen_range(20..84); // associated midi keynumber -> 60 == 'C4'
-                                                           // setting stage for midi callback to take a number to generate a tone
-                                                           // todo: use channels
-    let note = Key {
-        state: true,
-        keynumber: key,
-    };
+    let mut midi_in = MidiInput::new("midir reading input")?;
 
+    let in_ports = midi_in.ports();
+    let in_port = match in_ports.len() {
+        0 => {
+            println!("Error: No input connection found. Press enter to quit.");
+            let error = Err("closing connections.").unwrap();
+            //
+            error
+        }
+        1 => {
+            println!(
+                "Choosing the only available input port: {}",
+                midi_in.port_name(&in_ports[0]).unwrap()
+            );
+            &in_ports[0]
+        }
+        _ => {
+            println!("\nAvailable input ports:");
+            for (i, p) in in_ports.iter().enumerate() {
+                println!("{}: {}", i, midi_in.port_name(p).unwrap());
+            }
+            print!("Please select input port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            in_ports
+                .get(input.trim().parse::<usize>()?) // investigate
+                .ok_or("invalid input port selected")
+                .unwrap()
+        }
+    };
+    midi_in.ignore(Ignore::None);
+    let in_port_name = midi_in.port_name(in_port)?;
+    let in_port = in_port.clone();
+
+    let _read_thread = spawn(
+        move || match read(&in_port_name, in_port, midi_in, note_chan.0) {
+            Ok(_) => (),
+            Err(err) => println!("Error: {}", err),
+        },
+    );
+
+    // let key: usize = rand::thread_rng().gen_range(20..84); // associated midi keynumber -> 60 == 'C4'
+    // setting stage for midi callback to take a number to generate a tone
+    // todo: use channels
+    // let note = Key {
+    //     state: true,
+    //     keynumber: key,
+    // };
+
+    let note = note_chan.1.recv()?;
     println!("{:?}", note);
 
     // Sending single note to thread to play until keypress is accepted in CLI
@@ -108,4 +156,43 @@ where
             *sample = value;
         }
     }
+}
+
+/// Reads midi input and sends the midi notes parsed from midi messages through
+/// channels to our output midi function operating in a separate thread.
+pub fn read(
+    in_port_name: &str,
+    in_port: MidiInputPort,
+    midi_in: MidiInput,
+    tx: Sender<Key>,
+) -> Result<(), Box<dyn Error>> {
+    println!("\nOpening input connection");
+    let input_connection = midi_in.connect(
+        &in_port,
+        "midir-read-input",
+        move |_, message, _| {
+            //println!("{:?} (len = {})", message, message.len());
+            let message = MidiMessage::try_from(message).unwrap(); //unwrapping message slice
+            if let NoteOn(_, note, _) = message {
+                let newkey = Key {
+                    state: true,
+                    keynumber: note as usize,
+                };
+                let _ = tx.send(newkey); // sending note value through channel
+            }
+        },
+        (),
+    )?;
+
+    println!(
+        "Input connection open, reading input from '{}' (press enter to stop input): ",
+        in_port_name
+    );
+
+    loop {
+        // fix ending condition later once note structure is set up
+    }
+    input_connection.close();
+    println!("Closing input connection");
+    Ok(())
 }
