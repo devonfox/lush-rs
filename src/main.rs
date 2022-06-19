74be3fa9ecafaea4656970c351d58f5f7423fb17
devonfox <devonhouston.fox@gmail.com>
2022-06-18 17:22:25 -0700

    slowly add cpal functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+
+use cpal::traits::{DeviceTrait, HostTrait};
+
+fn main() -> anyhow::Result<()> {
+    // Find default output host
+    let host = cpal::default_host();
+
+    // assign output device from host
+    let device = host.default_output_device().expect("failed to find output device");
+    println!("Output device: {:?}", device.name()?); // print result
+
+    // Return result
+    // TODO: read up on anyhow crate
+    Ok(())
 }
