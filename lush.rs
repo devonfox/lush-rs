0bd4696213f51f490aa90cd966670890a9cb91aa
foxdev <foxdev@pdx.edu>
2022-06-19 11:31:14 -0700

    added key state struct in lib

--- lush.rs
+++ lush.rs
@@ -5,6 +5,8 @@ use std::sync::mpsc::*;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread::spawn;
 
+use lush_rs::Key;
+
 fn main() -> anyhow::Result<()> {
     // Find default output host
     let host = cpal::default_host();
@@ -15,16 +17,24 @@ fn main() -> anyhow::Result<()> {
         .expect("failed to find output device");
     println!("Output device: {:?}", device.name()?); // print result
 
-    let end_chan: (Sender<()>, Receiver<()>) = channel();
+    let end_chan: (Sender<()>, Receiver<()>) = channel(); // end channel
 
     let config = device.default_output_config().unwrap();
     println!("Default output config: {:?}", config);
 
-    let keynumber: usize = rand::thread_rng().gen_range(20..84); // associated midi keynumber -> 60 == 'C4'
-                                                                 // setting stage for midi callback to take a number to generate a tone
-                                                                 // todo: use channels
+    let key: usize = rand::thread_rng().gen_range(20..84); // associated midi keynumber -> 60 == 'C4'
+                                                           // setting stage for midi callback to take a number to generate a tone
+                                                           // todo: use channels
+    let note = Key {
+        state: true,
+        keynumber: key,
+    };
 
-    let run_thread = spawn(move || run::<f32>(&device, &config.into(), keynumber, end_chan.1));
+    println!("{:?}", note);
+
+    // Sending single note to thread to play until keypress is accepted in CLI
+    let run_thread = spawn(move || run::<f32>(&device, &config.into(), note, end_chan.1));
+
     let mut input = String::new();
     let stdin = stdin();
     input.clear();
@@ -32,12 +42,15 @@ fn main() -> anyhow::Result<()> {
         Ok(_) => println!("Ending program..."),
         Err(err) => println!("Error: {}", err),
     }; // wait for next enter key press to end program
+
+    // Send end note
     let _ = end_chan.0.send(());
+
+    // Join thread
     match run_thread.join() {
         Ok(_) => (),
         Err(error) => println!("Error: {:?}", error),
     };
-
     // Return result
     // TODO: read up on anyhow crate
     Ok(())
@@ -46,7 +59,7 @@ fn main() -> anyhow::Result<()> {
 pub fn run<T>(
     device: &cpal::Device,
     config: &cpal::StreamConfig,
-    key: usize,
+    note: Key,
     rx: Receiver<()>,
 ) -> Result<(), anyhow::Error>
 where
@@ -57,14 +70,11 @@ where
     let channels = config.channels as usize;
     println!("Channels: {}", channels);
 
-    let freq = { 440.0 * (2.0_f32).powf((key as f32 - 69.0) / 12.0) };
+    let freq = { 440.0 * (2.0_f32).powf((note.keynumber as f32 - 69.0) / 12.0) };
 
     let mut sample_clock = 0f32;
     let mut next_value = move || {
         sample_clock += 1.0;
-        // if sample_clock % 1000.0 == 0.0 {
-        //     println!("{}", sample_clock);
-        // }
 
         // Sine calc
         // (sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()\
@@ -72,7 +82,7 @@ where
         let square = 4.0 * (freq * sample_clock / sample_rate).floor()
             - 2.0 * (2.0 * freq * sample_clock / sample_rate).floor()
             + 1.0;
-        square * 0.25 // Currently half amplitude
+        square * 0.55 // Currently half amplitude
     };
     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
     let stream = device.build_output_stream(
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+/// Describes state of a note
+#[derive(Debug)]
+pub struct Key {
+    pub state: bool,
+    pub keynumber: usize,
+}
